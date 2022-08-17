00b776b7edee7112f435f64e4a1b9e090dde1495
yukihirog <works@yukihirog.jp>
2022-08-17 17:33:56 +0900

    srcとdestのディレクトリを自動生成するように変更

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,5 +1,15 @@
+import fs from 'fs';
+import config from './config/index.js';
 import { default as gulp } from 'gulp';
 import gulptask from './gulptask/index.js';
+
+if (!fs.existsSync(config.src)) {
+  fs.mkdirSync(config.src, { recursive: true });
+}
+
+if (!fs.existsSync(config.dest)) {
+  fs.mkdirSync(config.dest, { recursive: true });
+}
 
 gulp.task('default', gulptask.bundle.dev);
 export default gulp.series('default')
